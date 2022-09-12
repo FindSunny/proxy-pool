9765df26ba58710e7047b1e1a27190a411844c44
suojianfei <suojianfei@bestseller.com.cn>
2022-09-13 00:27:49 +0800

    add headers

--- utils/ProxyUtils.js
+++ utils/ProxyUtils.js
@@ -92,7 +92,10 @@ const ProxyUtils = {
      */
     getProxyHtmlInfo: async function (url, encode = 'UTF-8') {
         let response = await axios.get(url, {
-            responseType: "arraybuffer"
+            responseType: "arraybuffer",
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+            }
         });
         let { data } = response;
         let utf8decoder = new TextDecoder(encode); // 关键步骤
